147eaf81be7dfd8ce7eabc21e009b9610bc91386
Pavel Vancurik <pavel.vancurik@accenture.com>
2019-04-17 13:48:59 +0200

    Also second task but without Unit Test

---
+++ EpiserverAlloy/Services/PricingService.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EpiserverAlloy.Services
+{
+    public class PricingService
+    {
+        private const double ONLINE_DISCOUNT = 1.5;
+
+        public double? GetInidividualPrice(double CataloguePrice, int percentOrganizationalDiscount, bool loggedUser, bool OnlineDiscount, int? maxDiscount)
+        {
+            double price = 0;
+
+            if (!loggedUser)
+            {
+                return null;
+            }
+
+            price = CataloguePrice - GetPercentPrice(CataloguePrice) * percentOrganizationalDiscount;
+
+            if (OnlineDiscount)
+            {
+                price = price - GetPercentPrice(CataloguePrice) * ONLINE_DISCOUNT;
+            }
+
+            if (maxDiscount > 0)
+            {
+                double maxDiscountedPrice = price - GetPercentPrice(CataloguePrice) * (double)maxDiscount;
+                if (price < maxDiscountedPrice)
+                {
+                    price = maxDiscountedPrice;
+                }
+            }
+
+            return price;
+        }
+
+        private double GetPercentPrice(double price)
+        {
+            return price / 100;
+        }
+    }
+}
